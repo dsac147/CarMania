70e2af518a5b8e8ca6733e335d41f5f95386084a
Divyanshu Sachan <divyanshu@Divyanshus-MacBook-Pro.local>
2023-12-25 15:21:04 +0530

    feat: Add car card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,165 @@
 import Image from "next/image";
 
-import { Hero, SearchBar } from "@/components";
+import { Hero, SearchBar , CarCard } from "@/components";
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
 
-export default function Home() {
+export default async function Home({ searchParams }: HomeProps) {
+  // const allCars = await fetchCars({
+  //   manufacturer: searchParams.manufacturer || "",
+  //   year: searchParams.year || 2022,
+  //   fuel: searchParams.fuel || "",
+  //   limit: searchParams.limit || 10,
+  //   model: searchParams.model || "",
+  // });
+
+  // console.log(allCars);
+
+  const allCars = [
+    {
+      city_mpg: 19,
+      class: "minivan",
+      combination_mpg: 22,
+      cylinders: 6,
+      displacement: 3.5,
+      drive: "fwd",
+      fuel_type: "gas",
+      highway_mpg: 28,
+      make: "honda",
+      model: "odyssey",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 21,
+      class: "midsize car",
+      combination_mpg: 24,
+      cylinders: 4,
+      displacement: 2.5,
+      drive: "awd",
+      fuel_type: "gas",
+      highway_mpg: 29,
+      make: "kia",
+      model: "stinger awd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 22,
+      class: "midsize car",
+      combination_mpg: 25,
+      cylinders: 4,
+      displacement: 2.5,
+      drive: "rwd",
+      fuel_type: "gas",
+      highway_mpg: 32,
+      make: "kia",
+      model: "stinger rwd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 17,
+      class: "midsize car",
+      combination_mpg: 20,
+      cylinders: 6,
+      displacement: 3.3,
+      drive: "awd",
+      fuel_type: "gas",
+      highway_mpg: 24,
+      make: "kia",
+      model: "stinger awd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 18,
+      class: "midsize car",
+      combination_mpg: 20,
+      cylinders: 6,
+      displacement: 3.3,
+      drive: "rwd",
+      fuel_type: "gas",
+      highway_mpg: 25,
+      make: "kia",
+      model: "stinger rwd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 19,
+      class: "minivan",
+      combination_mpg: 22,
+      cylinders: 6,
+      displacement: 3.5,
+      drive: "fwd",
+      fuel_type: "gas",
+      highway_mpg: 26,
+      make: "kia",
+      model: "carnival",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 19,
+      class: "small sport utility vehicle",
+      combination_mpg: 22,
+      cylinders: 6,
+      displacement: 3.5,
+      drive: "fwd",
+      fuel_type: "gas",
+      highway_mpg: 26,
+      make: "acura",
+      model: "mdx fwd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 26,
+      class: "small sport utility vehicle",
+      combination_mpg: 29,
+      cylinders: 4,
+      displacement: 2.5,
+      drive: "fwd",
+      fuel_type: "gas",
+      highway_mpg: 33,
+      make: "hyundai",
+      model: "tucson fwd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 25,
+      class: "small sport utility vehicle",
+      combination_mpg: 26,
+      cylinders: 4,
+      displacement: 1.5,
+      drive: "fwd",
+      fuel_type: "gas",
+      highway_mpg: 28,
+      make: "mitsubishi",
+      model: "eclipse cross 2wd",
+      transmission: "a",
+      year: 2022,
+    },
+    {
+      city_mpg: 26,
+      class: "small sport utility vehicle",
+      combination_mpg: 27,
+      cylinders: 4,
+      displacement: 1.5,
+      drive: "fwd",
+      fuel_type: "gas",
+      highway_mpg: 29,
+      make: "mitsubishi",
+      model: "eclipse cross es 2wd",
+      transmission: "a",
+      year: 2022,
+    },
+  ];
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -15,6 +172,26 @@ export default function Home() {
         <div className="home__filters">
           <SearchBar />
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            {/* <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            /> */}
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            {/* <p>{allCars?.message}</p> */}
+          </div>
+        )}
       </div>
     </main>
   );
---
+++ utils/index.ts
@@ -0,0 +1,36 @@
+import { FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
